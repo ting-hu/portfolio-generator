1ffe7626086efab1a7e4c100d6c3101d3da0e408
Xueting Hu(Ting) <87446061+ting-hu@users.noreply.github.com>
2021-09-13 21:26:18 -0500

    Completed the second issue

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) =>{
-    for(let i =0; i < profileDataArr.length;i++){
-        console.log(profileDataArr[i]);
-    }
-    
-    console.log("==========");
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-/*const addNums = (numOne, numTwo)=>{
-    console.log(numOne,numTwo);
-    return numOne+numTwo;
-}
-
-const sum = addNums(5,3);*/
-
-
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
